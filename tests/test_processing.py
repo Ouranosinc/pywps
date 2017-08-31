c60c0afab9197f1da38de534be7764e3bd66c588
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-08-31 16:39:28 +0200

    added processing default mode test

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -9,21 +9,44 @@
 
 import unittest
 
+from pywps.processing.basic import MultiProcessing
+
 from pywps import configuration
+import pywps.processing
+from pywps import Process
+from pywps.app import WPSRequest
+from pywps.app import WPSResponse
+from pywps import LiteralOutput
 
 
 class ProcessingTest(unittest.TestCase):
     """Processing test cases"""
 
     def setUp(self):
-        # self.database = configuration.get_config_value('logging', 'database')
-        pass
+        def handler(request, response):
+            response.outputs['output'].data = '42'
+            return response
 
-    def test_default(self):
+        self.uuid = 1234
+        self.dummy_process = Process(
+            handler=handler,
+            identifier='dummy',
+            title='Dummy Process',
+            outputs=[LiteralOutput('output', 'Output', data_type='string')])
+        self.wps_request = WPSRequest()
+        self.wps_response = WPSResponse(self.dummy_process, self.wps_request, self.uuid)
+
+    def test_default_mode(self):
         """Test pywps.formats.Format class
         """
-        self.assertEqual(configuration.get_config_value('processing', 'mode'), 'default')
-        # self.assertTrue(session)
+        self.assertEqual(configuration.get_config_value('processing', 'mode'),
+                         'default')
+        process = pywps.processing.Process(
+            process=self.dummy_process,
+            wps_request=self.wps_request,
+            wps_response=self.wps_response)
+        # process.start()
+        self.assertTrue(isinstance(process, MultiProcessing))
 
 
 def load_tests(loader=None, tests=None, pattern=None):
